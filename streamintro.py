82719361f3e40c952c3aeb374100d21bbf45b114
Tony <56795657+BlueSkeleton@users.noreply.github.com>
2023-03-20 11:00:50 +0000

    Update streamintro.py

--- streamintro.py
+++ streamintro.py
@@ -6,3 +6,4 @@ st.title('Hello Wilders, welcome to my application!')
 st.write("I enjoy to discover streamlit possibilities")
 
 df = pd.read_csv("https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv")
+st.dataframe(df)
